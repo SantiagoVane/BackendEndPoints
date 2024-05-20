92b9309d6adf975a779c9f85867c8e5310d5d78f
Sebasjaga <sebastian.jaramillo03@uceva.edu.co>
2024-05-20 08:14:39 -0500

    Se definió el Método Create_app

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 
 db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        from .routes import main as main_blueprint
+        app.register_blueprint(main_blueprint)
+
+        db.create_all()
+
+    return app
